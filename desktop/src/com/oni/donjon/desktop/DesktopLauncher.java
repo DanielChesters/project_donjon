11a70687fc516f56b27b40d72d3c1cce05e4a5b5
Daniel Chesters <daniel.chesters@gmail.com>
2014-05-18 16:37:06 +0200

    initial commit

---
+++ desktop/src/com/oni/donjon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.oni.donjon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.oni.donjon.DonjonGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DonjonGame(), config);
+	}
+}
