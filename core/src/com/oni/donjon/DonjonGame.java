9d8dea2be17ccd9f3388fdab32c4a78b6fea7e02
Daniel Chesters <daniel.chesters@gmail.com>
2014-05-19 22:58:53 +0200

    Add a simple control for the '@' character

--- core/src/com/oni/donjon/DonjonGame.java
+++ core/src/com/oni/donjon/DonjonGame.java
@@ -3,12 +3,11 @@ package com.oni.donjon;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-import java.awt.*;
 
 public class DonjonGame extends ApplicationAdapter {
     SpriteBatch batch;
@@ -18,7 +17,7 @@ public class DonjonGame extends ApplicationAdapter {
     int posY;
 
     @Override
-    public void create () {
+    public void create() {
         batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
         font = new BitmapFont();
@@ -28,9 +27,47 @@ public class DonjonGame extends ApplicationAdapter {
     }
 
     @Override
-    public void render () {
+    public void render() {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT)) {
+                posY += 10;
+            } else {
+                posY += 1;
+            }
+            if (posY > Gdx.graphics.getHeight()) {
+                posY = 0;
+            }
+        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT)) {
+                posY -= 10;
+            } else {
+                posY -= 1;
+            }
+            if (posY < 0) {
+                posY = Gdx.graphics.getHeight();
+            }
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT)) {
+                posX -= 10;
+            } else {
+                posX -= 1;
+            }
+            if (posX < 0) {
+                posX = Gdx.graphics.getWidth();
+            }
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_RIGHT)) {
+                posX += 10;
+            } else {
+                posX += 1;
+            }
+            if (posX > Gdx.graphics.getWidth()) {
+                posX = 0;
+            }
+        }
         batch.begin();
         batch.draw(img, 0, 0);
         font.draw(batch, "@", posX, posY);
