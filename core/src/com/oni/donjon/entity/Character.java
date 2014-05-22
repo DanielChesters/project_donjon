59c590c6dff7162c10acf8b1f1d6ceedcccf1b85
Daniel Chesters <daniel.chesters@gmail.com>
2014-05-22 20:34:44 +0200

    Replace x and y integers with a Vector2 for character position

--- core/src/com/oni/donjon/entity/Character.java
+++ core/src/com/oni/donjon/entity/Character.java
@@ -3,55 +3,43 @@ package com.oni.donjon.entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * @author Daniel Chesters (on 20/05/14).
  */
 public class Character {
-    private int x;
-    private int y;
+    private Vector2 position;
     BitmapFont font;
     SpriteBatch batch;
 
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
+    public Vector2 getPosition() {
+        return position;
     }
 
     public void addX(int x) {
-        this.x += x;
-        if (this.x > Gdx.graphics.getWidth()) {
-            this.x = 0;
-        } else if (this.x < 0) {
-            this.x = Gdx.graphics.getWidth();
+        position.x += x;
+        if (position.x > Gdx.graphics.getWidth()) {
+            position.x = 0;
+        } else if (position.x < 0) {
+            position.x = Gdx.graphics.getWidth();
         }
     }
 
     public void addY(int y) {
-        this.y += y;
-        if (this.y > Gdx.graphics.getHeight()) {
-            this.y = 0;
-        } else if (this.y < 0) {
-            this.y = Gdx.graphics.getHeight();
+        position.y += y;
+        if (position.y > Gdx.graphics.getHeight()) {
+            position.y = 0;
+        } else if (position.y < 0) {
+            position.y = Gdx.graphics.getHeight();
         }
     }
 
     @Override
     public String toString() {
         return "@{" +
-                "x=" + x +
-                ", y=" + y +
+                "x=" + position.x +
+                ", y=" + position.y +
                 '}';
     }
 
@@ -62,26 +50,22 @@ public class Character {
 
         Character character = (Character) o;
 
-        if (x != character.x) return false;
-        return y == character.y;
+        return position.equals(character.position);
 
     }
 
     @Override
     public int hashCode() {
-        int result = x;
-        result = 31 * result + y;
-        return result;
+        return position.hashCode();
     }
 
     public Character(BitmapFont font, SpriteBatch batch) {
         this.font = font;
         this.batch = batch;
-        this.x = Gdx.graphics.getWidth() / 2;
-        this.y = Gdx.graphics.getHeight() / 2;
+        position = new Vector2(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
     }
 
     public void drawCharacter() {
-        font.draw(batch, "@", x, y);
+        font.draw(batch, "@", position.x, position.y);
     }
 }
