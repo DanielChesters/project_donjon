29ed06b546fede907a669041d7b5565fee33feb2
Daniel Chesters <daniel.chesters@gmail.com>
2014-05-25 02:25:14 +0200

    Fix "if/else/for/while/do statements should always use curly braces" and "Statements should be on separate lines"

--- core/src/com/oni/donjon/entity/Character.java
+++ core/src/com/oni/donjon/entity/Character.java
@@ -43,8 +43,12 @@ public class Character {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Character character = (Character) o;
 
