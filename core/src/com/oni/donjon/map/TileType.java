75d3ed5811e72caa4dea2bfe760ba1835af0f193
Daniel Chesters <daniel.chesters@gmail.com>
2014-05-26 19:54:37 +0200

    Add textures and tile types for stair

--- core/src/com/oni/donjon/map/TileType.java
+++ core/src/com/oni/donjon/map/TileType.java
@@ -9,7 +9,9 @@ public enum TileType {
     WALL("textures/wall.png", true),
     DOOR_CLOSE("textures/door_close.png", true),
     DOOR_OPEN("textures/door_open.png", false),
-    GROUND("textures/ground.png", false);
+    GROUND("textures/ground.png", false),
+    STAIR_UP("textures/stair_up.png", true),
+    STAIR_DOWN("textures/stair_down.png", true);
 
     private Texture texture;
     private boolean block;
