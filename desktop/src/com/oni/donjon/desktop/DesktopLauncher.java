3bf00105c9599b45ec323984680488375fba4e14
Daniel Chesters <daniel.chesters@gmail.com>
2016-02-05 21:08:41 +0100

    Change to LWJGL3 backend

--- desktop/src/com/oni/donjon/desktop/DesktopLauncher.java
+++ desktop/src/com/oni/donjon/desktop/DesktopLauncher.java
@@ -2,16 +2,15 @@ package com.oni.donjon.desktop;
 
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.oni.donjon.DonjonGame;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.height = 600;
-        config.width = 800;
-        new LwjglApplication(new DonjonGame(), config);
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowedMode(800, 600);
+        new Lwjgl3Application(new DonjonGame(), config);
         Gdx.app.setLogLevel(Application.LOG_DEBUG);
     }
 }
