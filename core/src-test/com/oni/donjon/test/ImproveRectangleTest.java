2fcdb6ab063b65ca1d7271a9147bfbcbee03f4a1
Daniel Chesters <daniel.chesters@gmail.com>
2017-04-21 01:19:10 +0200

    :white_check_mark: Add a second test for ImproveRectangle Class
    Just a second test for improve branches code coverage.

--- core/src-test/com/oni/donjon/test/ImproveRectangleTest.java
+++ core/src-test/com/oni/donjon/test/ImproveRectangleTest.java
@@ -11,9 +11,16 @@ public class ImproveRectangleTest {
 
 
     @Test
-    public void testIntersects() {
+    public void testIntersectsTrue() {
         ImproveRectangle rectangle1 = new ImproveRectangle(1, 1, 3, 3);
         ImproveRectangle rectangle2 = new ImproveRectangle(2, 2, 3, 3);
         Assert.assertTrue(rectangle1.intersects(rectangle2));
+    }
+
+    @Test
+    public void testIntersectsFalse() {
+        ImproveRectangle rectangle1 = new ImproveRectangle(10, 10, 1, 1);
+        ImproveRectangle rectangle2 = new ImproveRectangle(5, 5, 3, 3);
+        Assert.assertFalse(rectangle1.intersects(rectangle2));
     }
 }
