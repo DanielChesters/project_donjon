8d489068fcd28914565d6c938b6517237e053f4d
Daniel Chesters <daniel.chesters@gmail.com>
2017-05-31 00:12:59 +0200

    :art: convert MapActor in kotlin

---
+++ core/src/com/oni/donjon/actor/MapActor.kt
@@ -0,0 +1,18 @@
+package com.oni.donjon.actor
+
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.oni.donjon.data.GameData
+import com.oni.donjon.map.Tile
+
+/**
+ * @author Daniel Chesters (on 25/05/14).
+ */
+class MapActor : Actor() {
+    override fun draw(batch: Batch?, alpha: Float) {
+        GameData.map.tiles.filter({ it.isKnow }).forEach { t ->
+            batch!!.draw(t.type.texture, t.rectangle.getX() * Tile.SIZE,
+                    t.rectangle.getY() * Tile.SIZE)
+        }
+    }
+}
