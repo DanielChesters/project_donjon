989832993735e216f6600a4a09759847f2e1bb35
Daniel Chesters <daniel.chesters@gmail.com>
2017-06-09 01:19:53 +0200

    :art: Fix a little warning in code

--- core/src/main/kotlin/com/oni/donjon/map/Tile.kt
+++ core/src/main/kotlin/com/oni/donjon/map/Tile.kt
@@ -58,12 +58,12 @@ class Tile {
         return worldBody
     }
 
-    override fun equals(o: Any?): Boolean {
-        if (this === o)
+    override fun equals(other: Any?): Boolean {
+        if (this === other)
             return true
-        if (o == null || javaClass != o.javaClass)
+        if (other == null || javaClass != other.javaClass)
             return false
-        val tile = o as Tile?
+        val tile = other as Tile?
         return isKnow == tile!!.isKnow &&
                 type === tile.type &&
                 rectangle == tile.rectangle &&
