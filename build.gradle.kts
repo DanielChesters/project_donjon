4654a66204b017a5b7f057477ed86055448fcab1
Daniel Chesters <daniel.chesters@gmail.com>
2019-04-24 21:49:59 +0200

    :art: Add detekt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,13 +9,15 @@ buildscript {
         mavenLocal()
         mavenCentral()
         jcenter()
-        maven (url = "https://oss.sonatype.org/content/repositories/snapshots/")
+        maven(url = "https://plugins.gradle.org/m2/")
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
     }
 
     dependencies {
         classpath("com.github.ben-manes:gradle-versions-plugin:$gradleVersionsPluginVersion")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:$sonarqubeVersion")
+        classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.0.0-RC14")
     }
 }
 
@@ -23,15 +25,13 @@ subprojects {
 
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "com.github.ben-manes.versions")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
 
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
     }
 
     version = "0.1"
-    /*ext {
-        appName = "Projet-donjon"
-    }*/
 
     repositories {
         mavenLocal()
@@ -45,5 +45,3 @@ subprojects {
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
 }
-
-
